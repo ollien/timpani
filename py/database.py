186e30f3224f7a2c506a179250fc15008bcc5f60
Nick Krichevsky <njk828@gmail.com>
2015-08-20 00:11:35 -0400

    Remove self from ConnectionManager and fix namerror

--- py/database.py
+++ py/database.py
@@ -6,7 +6,6 @@ import tables
 import os.path
 
 CONFIG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../configs/"))
-print(CONFIG_PATH)
 configs = configmanager.ConfigManager(configPath = CONFIG_PATH)
 config = configs["database"]
 
@@ -14,14 +13,14 @@ class ConnectionManager():
 	_connections = {}
 
 	@staticmethod
-	def addConnection(self, connection, connectionName = uuid.uuid4().hex):
+	def addConnection(connection, connectionName = uuid.uuid4().hex):
 		if type(connectionName) == str:
 			if type(connection) == DatabaseConnection:
-				_connections[connectionName] = connection
+				ConnectionManager._connections[connectionName] = connection
 				return connectionName
 			elif type(connection) == str:
 				c = DatabaseConnection(connection)
-				_connections[connectionName] = connection
+				ConnectionManager._connections[connectionName] = connection
 				return connectionName
 			else:
 				raise ValueError("connection must be of type str, not %s", type(connection))
@@ -29,26 +28,24 @@ class ConnectionManager():
 			raise ValueError("connectionName must be of type str, not %s", type(name))
 	
 	@staticmethod
-	def getConnection(self, connectionName):
+	def getConnection(connectionName):
 		if type(connectionName) == str:
 			try:
-				return _connections[connectionName]
+				return ConnectionManager._connections[connectionName]
 			except KeyError:
 				return None
 
 	@staticmethod	
-	def closeConnection(self, connectionName):
+	def closeConnection(connectionName):
 		if type(connectionName) == str:
-			_connections[connectionName].session.close()
-			_connections.close()
-			del _connections[connectionName]
+			ConnectionManager._connections[connectionName].session.close()
+			del ConnectionManager._connections[connectionName]
 		else:
 			raise ValueError("connectionName must be of type str, not %s", type(name))
 
 class DatabaseConnection():
 	def __init__(self, connectionString = config["connection_string"], createTables = True):
 		self.connectionString = connectionString
-		print(self.connectionString)
 		self.engine = sqlalchemy.create_engine(self.connectionString)
 		self._Session = sqlalchemy.orm.sessionmaker(bind = self.engine)
 		self.session = self._Session()
