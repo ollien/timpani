c2c0e55fee3aa335720fb524b9de7089128cf712
Nick Krichevsky <njk828@gmail.com>
2015-08-20 20:57:27 -0400

    Add useres table

--- py/tables.py
+++ py/tables.py
@@ -20,11 +20,20 @@ class Tag(Base):
 
 #Used to relate tags to posts.
 class TagRelation(Base):
-	__tablename__ = "tag_relation"
+	__tablename__ = "tag_relations"
 
 	id = sqlalchemy.Column(sqlalchemy.Integer, primary_key = True)
 	post_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey(Post.__table__.columns.id))
 	tag_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey(Tag.__table__.columns.id))
+
+class User(Base):
+	__tablename__ = "users"
+
+	id = sqlalchemy.Column(sqlalchemy.Integer, primary_key = True)
+	username = sqlalchemy.Column(sqlalchemy.String)
+	password = sqlalchemy.Column(sqlalchemy.String)
+	can_change_settings = sqlalchemy.column(sqlalchemy.Boolean)
+	can_write_posts = sqlalchemy.column(sqlalchemy.Boolean)
 
 #The Following tables will only be the ones added to the database
 ALL_TABLES = [Post, Tag, TagRelation]
