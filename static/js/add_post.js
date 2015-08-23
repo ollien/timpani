02a9656e278fbc347e5fd2212b6c93a316c1190e
Nick Krichevsky <njk828@gmail.com>
2015-08-23 18:35:17 -0400

    Change input dimensions

--- static/js/add_post.js
+++ static/js/add_post.js
@@ -1,9 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event){
 	var editorContainer = document.querySelector("textarea#post-body")
-	console.log(editorContainer)
 	var containerStyle = getComputedStyle(editorContainer)
-	console.log(containerStyle)
-	var editor = CKEDITOR.replace("post-body", {height: containerStyle.height})
+	var editor = CKEDITOR.replace("post-body", {width: containerStyle.width, height: containerStyle.height})
 	//editor.resize("80%", "20%")
 	hljs.initHighlightingOnLoad()
 })
