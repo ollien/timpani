cf33cbf4bfdf189c31d7b4239cbb21fa301a038f
Nick Krichevsky <njk828@gmail.com>
2015-08-28 17:42:00 -0400

    Change postObj to post

--- py/blog.py
+++ py/blog.py
@@ -4,14 +4,14 @@ import configmanager
 mainConnection = database.ConnectionManager.getConnection("main")
 
 def getPosts(connection = mainConnection):
-	posts = {} #Will be a dict formatted as such {postId: {postObj: $POST_OBJECT_FROM_DATABASE, tags: [$TAGS_FROM_DATABASE]}}
+	posts = {} #Will be a dict formatted as such {postId: {post: $POST_OBJECT_FROM_DATABASE, tags: [$TAGS_FROM_DATABASE]}}
 	postsAndTags = connection.session.query(database.tables.Post, database.tables.Tag).outerjoin(database.tables.TagRelation, database.tables.Tag)
 	for result in postsAndTags:
 		post, tag = result
 		if post.id in posts.keys():
 			posts[post.id]["tags"].append(tag)
 		else:
-			posts[post.id] = {"postObj": post, "tags": []}
+			posts[post.id] = {"post": post, "tags": []}
 			if tag != None:
 				posts[post.id]["tags"].append(tag)
 	return posts
