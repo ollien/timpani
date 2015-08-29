3b25c9e1de8d6744d27b0f55aea03db6df6046e1
Nick Krichevsky <njk828@gmail.com>
2015-08-29 17:14:04 -0400

    import flask instead of passing in request

--- py/webserver/webfunctions.py
+++ py/webserver/webfunctions.py
@@ -1,9 +1,11 @@
+import flask
 import auth
+import urllib.parse
 
 def checkForSession(request):
+	print(flask.request == request)
 	if "sessionId" in request.cookies:
 		session = auth.validateSession(request.cookies["sessionId"])
 		if session != None:
 			return session
 	return None
-
