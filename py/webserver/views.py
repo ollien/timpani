431e0aec97385158d8db12bf6700e87540ca3ff1
Nick Krichevsky <njk828@gmail.com>
2015-08-29 15:25:46 -0400

    Add homepage to flask

--- py/webserver/views.py
+++ py/webserver/views.py
@@ -3,12 +3,19 @@ import os.path
 import database
 import auth
 import blog
+import configmanager
 
 FILE_LOCATION = os.path.abspath(os.path.dirname(__file__))
-TEMPLATE_ROOT = os.path.abspath(os.path.join(FILE_LOCATION, "../templates"))
-blueprint = flask.Blueprint("blogViews", __name__, template_folder = TEMPLATE_ROOT)
+CONFIG_PATH = os.path.abspath(os.path.join(FILE_LOCATION, "../../configs/"))
+TEMPLATE_PATH = os.path.abspath(os.path.join(FILE_LOCATION, "../../templates"))
+
+configs = configmanager.ConfigManager(configPath = CONFIG_PATH)
+templateConfig = configs["templates"]
+print(TEMPLATE_PATH)
+blueprint = flask.Blueprint("blogViews", __name__, template_folder = TEMPLATE_PATH)
 
 @blueprint.route("/")
-def hello_world():
-	return "Hello World"
-
+def show_posts():
+	posts = blog.getPosts()
+	getPostAuthor = blog.getAuthorFullname if templateConfig["display_full_name"] else blog.getAuthorUsername
+	return flask.render_template("posts.html", posts = posts, getPostAuthor = getPostAuthor)
--- py/webserver/webserver.py
+++ py/webserver/webserver.py
@@ -4,13 +4,11 @@ import datetime
 import urllib.parse
 import configmanager
 from . import views
+
 FILE_LOCATION = os.path.abspath(os.path.dirname(__file__))
-STATIC_ROOT = os.path.abspath(os.path.join(FILE_LOCATION, "../../static"))
+STATIC_PATH = os.path.abspath(os.path.join(FILE_LOCATION, "../../static"))
 CONFIG_PATH = os.path.abspath(os.path.join(FILE_LOCATION, "../../configs/"))
 
-#configs = configmanager.ConfigManager(os.path.abspath(os.path.join(FILE_LOCATION, "configs")))
-#templateConfig = configs["templates"]
-
-app = flask.Flask(__name__, static_folder = STATIC_ROOT)	
+app = flask.Flask(__name__, static_folder = STATIC_PATH)
 app.register_blueprint(views.blueprint)
 app.debug = True
