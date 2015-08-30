2f2b4febdccfd30338e727aa21c5b65dc5ca9dfb
Nick Krichevsky <njk828@gmail.com>
2015-08-30 19:31:41 -0400

    Add required fields to both Title and post body

--- static/js/add_post.js
+++ static/js/add_post.js
@@ -4,10 +4,17 @@ document.addEventListener("DOMContentLoaded", function(event){
 	var editor = CKEDITOR.replace("post-body", {width: containerStyle.width, height: containerStyle.height})
 	//editor.resize("80%", "20%")
 	hljs.initHighlightingOnLoad()
+	var validityInput = document.getElementById("post-validity")
 	var tagsInput = document.getElementById("tags-input")
 	var placeholderTagsInput = document.getElementById("placeholder-tags-input")
 	var tagsInputPlugin = insignia(tagsInput);
 	var form = document.getElementById("post-form") 
+
+	//Displays a message when CKEditor is not filled out;
+	editor.on("required", function(event){
+		event.cancel();
+		validityInput.setCustomValidity("Please fill out this field.")
+	})
 
 	tagsInput.addEventListener("focus", function(event){
 		var div = document.getElementById("tag-input-div")
