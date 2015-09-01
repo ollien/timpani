6671aa33371676b89993decc1461b34977496c11
Nick Krichevsky <njk828@gmail.com>
2015-09-01 01:19:48 -0400

    add post request for deleting post. (404s)

--- static/js/manage_posts.js
+++ static/js/manage_posts.js
@@ -1,14 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(event){
-	var modal = new Modal(".modal")
+	var modalElement = document.querySelector(".modal")
+	var modal = new Modal(modalElement)
 	modal.element.addEventListener("positive-pressed", function(event){
-		alert("pressed");	
+		var postId = modal.element.getAttribute("post-id")
+		var request = new XMLHttpRequest();
+		request.open("POST", "/delete_post/"+postId)
+		request.send();
 	})
 
 	var deleteButtons = document.querySelectorAll("span.button.delete")
 	var deletePostTitle = document.querySelector("span.delete-post-title")
 	Array.prototype.slice.call(deleteButtons).forEach(function(button){
 		button.addEventListener("click", function(event){
-			var title = button.parentNode.parentNode.querySelector("span.post-title").textContent
+			var li = button.parentNode.parentNode;
+			var title = li.querySelector("span.post-title").textContent
+			modalElement.setAttribute("post-id", li.getAttribute("post-id"))
 			deletePostTitle.textContent = title
 			modal.show();
 		})	
