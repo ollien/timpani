c8222770d3499390850507c6c8414447e03a8e19
Nick Krichevsky <njk828@gmail.com>
2015-09-03 17:03:18 -0400

    Remove ckeditor stuff from add_post.js

--- static/js/add_post.js
+++ static/js/add_post.js
@@ -1,20 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(event){
-	var editorContainer = document.querySelector("textarea#post-body")
-	var containerStyle = getComputedStyle(editorContainer)
-	var editor = CKEDITOR.replace("post-body", {width: containerStyle.width, height: containerStyle.height})
-	//editor.resize("80%", "20%")
-	hljs.initHighlightingOnLoad()
 	var validityInput = document.getElementById("post-validity")
 	var tagsInput = document.getElementById("tags-input")
 	var placeholderTagsInput = document.getElementById("placeholder-tags-input")
 	var tagsInputPlugin = insignia(tagsInput);
 	var form = document.getElementById("post-form") 
-
-	//Displays a message when CKEditor is not filled out;
-	editor.on("required", function(event){
-		event.cancel();
-		validityInput.setCustomValidity("Please fill out this field.")
-	})
 
 	tagsInput.addEventListener("focus", function(event){
 		var div = document.getElementById("tag-input-div")
