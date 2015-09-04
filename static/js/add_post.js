423a7e462ec86871b9c2c94ec40724b1b278b94d
Nick Krichevsky <njk828@gmail.com>
2015-09-03 21:20:32 -0400

    Add partial link adding support

--- static/js/add_post.js
+++ static/js/add_post.js
@@ -9,11 +9,33 @@ document.addEventListener("DOMContentLoaded", function(event){
 	var linkButton = document.getElementById("add-link")
 	var linkModalElement = document.getElementById("link-modal")
 	var linkModal = new Modal(linkModalElement)
+	var modalInput = linkModalElement.querySelector("input#modal-link")	
 
 	editor.addModule("toolbar", {container: "div#toolbar"})
 
 	linkButton.addEventListener("click", function(event){
+		//Once the input is focused, we will lose our selection. We need to get it now.
 		linkModal.show()
+		modalInput.focus()
+	})
+
+	linkModal.element.addEventListener("positive-pressed", function(event){
+		if (modalInput.value.length === 0) {
+			modalInput.setCustomValidity("Please enter a link")	
+			event.preventDefault()
+		}
+		else {
+			editor.focus()
+			var selection = editor.getSelection()
+			editor.setSelection(null)
+			//This basically indicates that we don't actually have a selection.
+			if (selection.end - selection.start === 0){
+				console.log(modalInput.value)
+				console.log(selection.start)
+				console.log(modalInput.value)
+				editor.insertText(selection.start, modalInput.value, "link", modalInput.value)	
+			}
+		}
 	})
 
 	tagsInput.addEventListener("focus", function(event){
