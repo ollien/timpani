4857a850120983967b5f87c76ab885480bb95412
Nick Krichevsky <njk828@gmail.com>
2015-09-10 12:32:13 -0400

    Run login.js through jshint

--- static/js/login.js
+++ static/js/login.js
@@ -1,17 +1,19 @@
-document.addEventListener("DOMContentLoaded", function(event){
-	var form = document.getElementById("login-form")
-	var usernameInput = document.getElementById("username-field")
-	var passwordInput = document.getElementById("password-field")
+/*jshint eqnull: true */
+document.addEventListener('DOMContentLoaded', function (event) {
+		var form = document.getElementById('login-form');
+		var usernameInput = document.getElementById('username-field');
+		var passwordInput = document.getElementById('password-field');
 
-	var addInvalid = function(event){
-		this.classList.add("error")	
-	}
-	var resetInvalid = function(event){
-		this.classList.remove("error")
-	}
+		var addInvalid = function (event) {
+				this.classList.add('error');
+		};
 
-	usernameInput.addEventListener("invalid", addInvalid);
-	passwordInput.addEventListener("invalid", addInvalid);
-	usernameInput.addEventListener("input", resetInvalid);
-	passwordInput.addEventListener("input", resetInvalid);
-})
+		var resetInvalid = function (event) {
+				this.classList.remove('error');
+		};
+
+		usernameInput.addEventListener('invalid', addInvalid);
+		passwordInput.addEventListener('invalid', addInvalid);
+		usernameInput.addEventListener('input', resetInvalid);
+		passwordInput.addEventListener('input', resetInvalid);
+});
