161142d7994a2ef3c573b28dd2b31aaedac7b5e8
Nick Krichevsky <njk828@gmail.com>
2015-09-10 13:15:49 -0400

    Add js task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,14 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var autoPrefixer = require("gulp-autoprefixer");
 var minifyCss = require("gulp-minify-css");
-var ugilfy = require("gulp-uglify");
+var jshint = require("gulp-jshint");
+var stylishJshint = require("jshint-stylish");
+var uglify = require("gulp-uglify");
 
 var SASS_SRC = "./scss/*.scss";
 var SASS_DEST = "./static/css";
+var JS_SRC = "./js/*.js"
+var JS_DEST = "./static/js"
 
 gulp.task("sass", function () {
 	return gulp.src(SASS_SRC).pipe(sass())
@@ -18,6 +22,15 @@ gulp.task("sass", function () {
 		.pipe(minifyCss())
 		.pipe(gulp.dest(SASS_DEST));
 });
+
+gulp.task("js", function() {
+	return gulp.src(JS_SRC)
+		.pipe(jshint())
+		.pipe(jshint.reporter(stylishJshint))
+		.pipe(jshint.reporter("fail"))
+		.pipe(uglify())	
+		.pipe(gulp.dest(JS_DEST));
+})
 
 gulp.task("watch", function () {
 	//Watch Sass for changes.
