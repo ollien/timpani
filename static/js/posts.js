7843f7eee1611a0d1f26c3eb9ab314c59feef2b6
Nick Krichevsky <njk828@gmail.com>
2015-09-10 12:38:50 -0400

    Run jshint on posts.js

--- static/js/posts.js
+++ static/js/posts.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded", function(event){
-	var code = document.querySelectorAll("[class*=language-");
-	Array.prototype.slice.call(code).forEach(function(element){
-		console.log(element)
-		Prism.highlightElement(element)	
-	})
-})
+/*jshint eqnull: true */
+document.addEventListener('DOMContentLoaded', function (event) {
+	var code = document.querySelectorAll('[class*=language-');
+	Array.prototype.slice.call(code).forEach(function (element) {
+		Prism.highlightElement(element);
+	});
+});
