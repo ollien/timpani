6714724e72c1e887dbc02df17074a85d711720e0
Nick Krichevsky <njk828@gmail.com>
2015-09-10 09:09:57 -0400

    Add watch task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,24 @@ var autoPrefixer = require("gulp-autoprefixer")
 var minifyCss = require("gulp-minify-css")
 var ugilfy = require("gulp-uglify")
 
+var SASS_SRC = "./scss/*.scss"
+var SASS_DEST = "./static/css"
+
 gulp.task("sass", function(){
-	return gulp.src("./scss/*.scss")
+	return gulp.src(SASS_SRC)
 		.pipe(sass())
 		.pipe(autoPrefixer({browsers: ["last 2 versions", "IE 9"]}))
 		.pipe(minifyCss())
-		.pipe(gulp.dest("./static/css/"))
+		.pipe(gulp.dest(SASS_DEST))
+})
+
+gulp.task("watch", function(){
+	//Watch Sass for changes.
+	var sassWatch = gulp.watch(SASS_SRC, ["sass"])
+	sassWatch.on("change", function(event){
+		var change_type = event.type;
+		change_type[0] = change_type[0].toUpperCase()
+		console.log(change_type + " detected on "+ event.path + ". Running sass.")
+	})
+
 })
