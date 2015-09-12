7eb1a7f39e3f1a2a71562c86eb0a8cb0c162ba86
Nick Krichevsky <njk828@gmail.com>
2015-09-12 15:54:32 -0400

    Rework folder structure one last time

--- src/py/timpani.py
+++
@@ -1,9 +0,0 @@
-import webserver
-
-def start():
-	#init webserver
-	server = webserver.WebServer()
-	application = server.run()
-
-if __name__ == "__main__":
-	start()
---
+++ timpani/__init__.py
@@ -0,0 +1,4 @@
+from . import database
+from . import blog
+from . import auth
+from . import configmanager
