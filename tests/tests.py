fe22f26a317c944b2f73864d349f098361c9f18f
Nick Krichevsky <njk828@gmail.com>
2015-09-12 16:44:10 -0400

    Fix ArgumentError with driver in tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -3,4 +3,4 @@ import tests
 
 driver = webdriver.Chrome() #Will be replaced with selenium once test dev is finished
 
-tests.login.test("tests", "password")
+tests.login.test(driver, "tests", "password")
