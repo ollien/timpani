9efeba679dc1ad1ac45ff4b49cacb8467cfdf8b8
Nick Krichevsky <njk828@gmail.com>
2015-09-12 19:40:27 -0400

    Add travis support for saucelabs

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,12 @@
 from selenium import webdriver
 import tests
 
-driver = webdriver.Chrome() #Will be replaced with selenium once test dev is finished
+username = os.environ["SAUCE_USERNAME"]
+access_key = os.environ["SAUCE_ACCESS_KEY"]
+capabilities["tunnel-identifier"] = os.environ["TRAVIS_JOB_NUMBER"]
+capabilities["build"] = os.environ["TRAVIS_BUILD_NUMBER"]
+url = "%s:%s@localhost:4445" % (username, access_key)
+driver = webdriver.Remote(desired_capabilities=capabilities, command_executor="http://%s/wd/hub" % url)
 
 tests.login.test(driver, "tests", "password")
 driver.add_cookie({"name": "sessionId", "value": ""})
