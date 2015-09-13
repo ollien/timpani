9cf4c91fe10770c60e8fe2444aa2c95e79e00660
Nick Krichevsky <njk828@gmail.com>
2015-09-12 20:56:03 -0400

    Remove docker stop and start from travis

--- tests/travis-setup.sh
+++ tests/travis-setup.sh
@@ -9,5 +9,3 @@ echo '{"connection_string": "postgresql://root@127.0.0.1/blog"}' > configs/datab
 echo '{"display_full_name": true}' > configs/templates.json
 cd $(dirname "$BASH_SOURCE")
 python3 ./travis-setup.py
-
-docker stop postgres
