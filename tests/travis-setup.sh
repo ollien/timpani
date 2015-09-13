3fd4758a3435deff93e681b5ded21640c929878e
Nick Krichevsky <njk828@gmail.com>
2015-09-12 20:58:48 -0400

    Change postgres port

--- tests/travis-setup.sh
+++ tests/travis-setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 docker pull ollien/postgres
-docker run -d --name="postgres" -p 127.0.0.01:5432:5432 -p 127.0.0.1:5433:5433 postgres
+docker run -d --name="postgres" -p 127.0.0.01:54320:5432 -p 127.0.0.1:54330:5433 postgres
 docker start postgres
 
 mkdir configs
-echo '{"connection_string": "postgresql://root@127.0.0.1/blog"}' > configs/database.json
+echo '{"connection_string": "postgresql://root@127.0.0.1:54320/blog"}' > configs/database.json
 echo '{"display_full_name": true}' > configs/templates.json
 cd $(dirname "$BASH_SOURCE")
 python3 ./travis-setup.py
