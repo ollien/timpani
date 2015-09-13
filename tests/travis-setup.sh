47131a48547de36e43c77acb00205fdf911c3c19
Nick Krichevsky <njk828@gmail.com>
2015-09-12 21:44:32 -0400

    Create database in travis-setup.sh

--- tests/travis-setup.sh
+++ tests/travis-setup.sh
@@ -3,6 +3,7 @@
 docker pull ollien/postgres
 docker run -d --name="postgres" -p 127.0.0.01:54320:5432 -p 127.0.0.1:54330:5433 postgres
 docker start postgres
+docker exec postgres psql postgres -c "CREATE DATABASE blog"
 
 mkdir configs
 echo '{"connection_string": "postgresql://root@127.0.0.1:54320/blog"}' > configs/database.json
