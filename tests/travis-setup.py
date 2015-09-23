3c163c85efe1abf0fdd05306fd3f8c8e4d410058
Nick Krichevsky <njk828@gmail.com>
2015-09-23 19:21:16 -0400

    Use createUser method in travis-setup

--- tests/travis-setup.py
+++ tests/travis-setup.py
@@ -2,11 +2,7 @@ import bcrypt
 import sys
 import os
 sys.path.insert(0, "..")
-from timpani import database
-
-connection = database.DatabaseConnection()
+import timpani
 hashedpassword = bcrypt.hashpw(bytes("password", "utf-8"), bcrypt.gensalt()).decode("utf-8")
-user = database.tables.User(username = "tests", password = hashedpassword, full_name = "Timpani Tests", can_change_settings = True, can_write_posts = True)
-connection.session.add(user)
-connection.session.commit()
+timpani.auth.createUser("tests", hashedPassword, True, True)
 connection.close()
