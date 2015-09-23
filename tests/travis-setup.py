d2d736311dce8d5685a8895a7c106bb65392c3f6
Nick Krichevsky <njk828@gmail.com>
2015-09-23 19:26:29 -0400

    Fix NameError and database connection errors in travis-setup.py

--- tests/travis-setup.py
+++ tests/travis-setup.py
@@ -1,8 +1,10 @@
 import bcrypt
 import sys
-import os
 sys.path.insert(0, "..")
 import timpani
-hashedpassword = bcrypt.hashpw(bytes("password", "utf-8"), bcrypt.gensalt()).decode("utf-8")
+
+connection = timpani.database.DatabaseConnection())
+timpani.database.ConnectionManager.addConnection("main", connection)
+hashedPassword = bcrypt.hashpw(bytes("password", "utf-8"), bcrypt.gensalt()).decode("utf-8")
 timpani.auth.createUser("tests", hashedPassword, True, True)
 connection.close()
