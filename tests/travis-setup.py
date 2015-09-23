2a579ca27e8af3c2efacffe878024121e18be48d
Nick Krichevsky <njk828@gmail.com>
2015-09-23 19:27:52 -0400

    Fix syntax errors and argument order errors in travis-setup

--- tests/travis-setup.py
+++ tests/travis-setup.py
@@ -3,8 +3,8 @@ import sys
 sys.path.insert(0, "..")
 import timpani
 
-connection = timpani.database.DatabaseConnection())
-timpani.database.ConnectionManager.addConnection("main", connection)
+connection = timpani.database.DatabaseConnection()
+timpani.database.ConnectionManager.addConnection(connection, "main")
 hashedPassword = bcrypt.hashpw(bytes("password", "utf-8"), bcrypt.gensalt()).decode("utf-8")
 timpani.auth.createUser("tests", hashedPassword, True, True)
 connection.close()
