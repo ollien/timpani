9844c32b37ebf46ae60234b881993ae625cbe4b8
Nick Krichevsky <njk828@gmail.com>
2015-09-23 19:41:44 -0400

    Fix syntax error in travis-setup.py

--- tests/travis-setup.py
+++ tests/travis-setup.py
@@ -6,5 +6,5 @@ import timpani
 connection = timpani.database.DatabaseConnection()
 timpani.database.ConnectionManager.addConnection(connection, "main")
 hashedPassword = bcrypt.hashpw(bytes("password", "utf-8"), bcrypt.gensalt()).decode("utf-8")
-timpani.auth.createUser("tests", "Timpani Tests" hashedPassword, True, True)
+timpani.auth.createUser("tests", "Timpani Tests", hashedPassword, True, True)
 connection.close()
