32ee6eea576d30acf2ada837ee65a2e6ab25edc0
Nick Krichevsky <njk828@gmail.com>
2015-10-04 14:17:55 -0400

    Add getAllSettings function to settings.py

--- timpani/settings.py
+++ timpani/settings.py
@@ -1,5 +1,11 @@
 from . import database
 
+def getAllSettings():
+	databaseConnection = database.ConnectionManager.getConnection("main")
+	query = databaseConnection.session.query(database.tables.Setting)
+	settings = query.all()
+	return {setting.name: setting.value for setting in settings}
+
 def getSettingValue(name):
 	databaseConnection = database.ConnectionManager.getConnection("main")
 	query = databaseConnection.session.query(database.tables.Setting).filter(database.tables.Setting.name == name)
