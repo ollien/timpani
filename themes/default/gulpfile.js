f514a5d63aafa13d7b31842b02e64286ba011169
Nick Krichevsky <njk828@gmail.com>
2015-10-25 02:09:04 -0400

    Add default theme gulpfile

---
+++ themes/default/gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var autoPrefixer = require("gulp-autoprefixer");
+var minifyCss = require("gulp-minify-css");
+var rename = require("gulp-rename");
+var plumber = require("gulp-plumber");
+
+gulp.task("default-theme", function(){
+	return gulp.src("./src.scss")
+		.pipe(plumber())
+		.pipe(sass().on("error", sass.logError))
+		.pipe(autoPrefixer({
+			browsers: [
+				"last 2 versions",
+				"IE 9"
+			]
+		}))
+		.pipe(minifyCss())
+		.pipe(rename("theme.css"))
+		.pipe(gulp.dest("./"));
+});
