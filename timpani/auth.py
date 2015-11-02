c0e1d10953f539dd6b3f68f607005e8e748f82f4
Nick Krichevsky <njk828@gmail.com>
2015-11-01 19:38:31 -0500

    Fix lines in auth.py to to be 79 chars or less

--- timpani/auth.py
+++ timpani/auth.py
@@ -19,12 +19,22 @@ CAN_POST_PERMISSION = "can_write_posts"
 def createUser(username, full_name, password, can_change_settings, can_write_posts):
 	username = username.lower()
 	passwordAsBytes = bytes(password, "utf-8")
-	passwordHash = bcrypt.hashpw(passwordAsBytes, bcrypt.gensalt(rounds = BCRYPT_ROUNDS)).decode("utf-8")
+	passwordHash = bcrypt.hashpw(passwordAsBytes, 
+		bcrypt.gensalt(rounds = BCRYPT_ROUNDS))
+	passwordHash = passwordHash.decode("utf-8")
 	databaseConnection = database.ConnectionManager.getConnection("main")
-	query = databaseConnection.session.query(database.tables.User).filter(database.tables.User.username == username)
+	query = databaseConnection.session
+		.query(database.tables.User)
+		.filter(database.tables.User.username == username)
 
 	if query.count() == 0:
-		userObject = database.tables.User(username = username, full_name = full_name, password = passwordHash, can_change_settings = can_change_settings, can_write_posts = can_write_posts)
+		userObject = database.tables.User(
+			username = username,
+			full_name = full_name,
+			password = passwordHash,
+			can_change_settings = can_change_settings,
+			can_write_posts = can_write_posts)
+
 		databaseConnection.session.add(userObject)
 		databaseConnection.session.commit()
 	else:
@@ -34,7 +44,9 @@ def validateUser(username, password):
 	username = username.lower()
 	passwordAsBytes = bytes(password, "utf-8")
 	databaseConnection = database.ConnectionManager.getConnection("main")
-	query = databaseConnection.session.query(database.tables.User).filter(database.tables.User.username == username)
+	query = databaseConnection.session
+		.query(database.tables.User)
+		.filter(database.tables.User.username == username)
 
 	if query.count() > 0:
 		userObject = query.first()
@@ -46,27 +58,37 @@ def validateUser(username, password):
 
 def userHasPermission(username, permissionName):
 	databaseConnection = database.ConnectionManager.getConnection("main")
-	query = databaseConnection.session.query(database.tables.User).filter(database.tables.User.username == username)
+	query = databaseConnection.session
+		.query(database.tables.User)
+		.filter(database.tables.User.username == username)
+
 	if query.count() > 0:
 		userObj = query.first()
 		return getattr(userObj, permissionName)
 	return False
 
 def generateSessionId():
-	return binascii.hexlify(os.urandom(authConfig["session_id_length"])).decode("utf-8")
+	sessionId = binascii.hexlify(os.urandom(authConfig["session_id_length"]))
+	return sessionId.decode("utf-8")
 
 def createSession(username, sessionId = None):
 	if sessionId == None:
 		sessionId = generateSessionId()
 	username = username.lower()
 	databaseConnection = database.ConnectionManager.getConnection("main")
-	query = databaseConnection.session.query(database.tables.User).filter(database.tables.User.username == username)
+	query = databaseConnection.session
+		.query(database.tables.User)
+		.filter(database.tables.User.username == username)
 
 	if query.count() > 0:
 		userObject = query.first()
 		userId = userObject.id
-		expires = datetime.datetime.now() + datetime.timedelta(weeks = 2) #Now plus two weeks.
-		sessionObj = database.tables.Session(user_id = userId, session_id = sessionId, expires = expires)
+		#Set expiry to the date of now + two weeks
+		expires = datetime.datetime.now() + datetime.timedelta(weeks = 2)
+		sessionObj = database.tables.Session(
+			user_id = userId, 
+			session_id = sessionId, 
+			expires = expires)
 		databaseConnection.session.add(sessionObj)
 		databaseConnection.session.commit()
 		return (sessionId, expires)
@@ -75,13 +97,17 @@ def createSession(username, sessionId = None):
 
 def validateSession(sessionId):
 	databaseConnection = database.ConnectionManager.getConnection("main")
-	query = databaseConnection.session.query(database.tables.Session).filter(database.tables.Session.session_id == sessionId)
+	query = databaseConnection.session
+		.query(database.tables.Session)
+		.filter(database.tables.Session.session_id == sessionId)
 
 	if query.count() > 0:
 		sessionObj = query.first()
 		userId = sessionObj.user_id
 		if datetime.datetime.now() < sessionObj.expires:
-			userQuery = databaseConnection.session.query(database.tables.User).filter(database.tables.User.id == userId)
+			userQuery = databaseConnection.session
+				.query(database.tables.User)
+				.filter(database.tables.User.id == userId)
 			if userQuery.count() > 0:
 				return sessionObj
 
@@ -89,4 +115,8 @@ def validateSession(sessionId):
 
 def invalidateSession(sessionId):
 	databaseConnection = database.ConnectionManager.getConnection("main")
-	query = databaseConnection.session.query(database.tables.Session).filter(database.tables.Session.session_id == sessionId).delete(synchronize_session=False)
+	#Delete the session from the database
+	query = databaseConnection.session
+		.query(database.tables.Session)
+		.filter(database.tables.Session.session_id == sessionId)
+		.delete(synchronize_session=False)
