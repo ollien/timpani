562a3bcf1798511b13e7b3bd35316898bfcc24be
Nick Krichevsky <njk828@gmail.com>
2015-11-06 21:49:26 -0500

    Correct theme path in themes.py

--- timpani/themes.py
+++ timpani/themes.py
@@ -1,3 +1,3 @@
 from . import database
 
-THEME_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../themes"))
+THEME_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../themes"))
