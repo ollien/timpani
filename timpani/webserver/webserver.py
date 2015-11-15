acec9342e392fed103e5d6b78470251d2cf535d6
Nick Krichevsky <njk828@gmail.com>
2015-11-15 01:39:51 -0500

    Add teardown request instead of usesDatabase decorator

--- timpani/webserver/webserver.py
+++ timpani/webserver/webserver.py
@@ -2,6 +2,7 @@ import flask
 import os.path
 import datetime
 import urllib.parse
+from .. import database
 from .. import configmanager
 from . import controllers
 
@@ -16,3 +17,8 @@ app = flask.Flask(__name__, static_folder = STATIC_PATH)
 app.secret_key = authConfig["signing_key"]
 app.register_blueprint(controllers.user.blueprint)
 app.register_blueprint(controllers.admin.blueprint)
+
+@app.teardown_request
+def teardown_request(exception = None):
+	databaseConnection = database.ConnectionManager.getConnection("main")
+	databaseConnection.session.close()
