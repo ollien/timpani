b66ea5f0c9d161c751e9dcede30d4ff2029d2f93
Nick Krichevsky <njk828@gmail.com>
2016-01-25 13:21:00 -0500

    Add deleteUserByUsername method

--- timpani/auth.py
+++ timpani/auth.py
@@ -106,6 +106,15 @@ def deleteUserById(user_id):
 	if query.count() > 0:
 		deleteUser(query.first())
 
+def deleteUserByUsername(username):
+	databaseConnection = database.ConnectionManager.getMainConnection()
+	query = (databaseConneciton.session
+		.query(database.tables.user)
+		.filter(database.tables.id(username))
+	#We don't have to worry about checking if it's >1 because id is a primary key.
+	if query.count() > 0:
+		deleteUser(query.first())
+
 def generateSessionId():
 	sessionId = binascii.hexlify(os.urandom(authConfig["session_id_length"]))
 	return sessionId.decode("utf-8")
