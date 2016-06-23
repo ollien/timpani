b9d3fc49c2626f10cc4e652793e7b341d155f695
Nick Krichevsky <njk828@gmail.com>
2016-06-23 13:38:57 -0400

    Add xssFilter as a jinja filter

--- timpani/webserver/webserver.py
+++ timpani/webserver/webserver.py
@@ -5,6 +5,7 @@ import urllib.parse
 from .. import database
 from .. import configmanager
 from . import controllers
+from .webhelpers import xssFilter
 
 FILE_LOCATION = os.path.abspath(os.path.dirname(__file__))
 STATIC_PATH = os.path.abspath(os.path.join(FILE_LOCATION, "../../static"))
@@ -17,6 +18,7 @@ app = flask.Flask(__name__, static_folder=STATIC_PATH)
 app.secret_key = authConfig["signing_key"]
 app.register_blueprint(controllers.user.blueprint)
 app.register_blueprint(controllers.admin.blueprint)
+app.jinja_env.filters["xssFilter"] = xssFilter
 
 @app.teardown_request
 def teardown_request(exception=None):
