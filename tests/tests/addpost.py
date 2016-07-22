996375a8161e3669ed9b5c2dc8de54fd3252bfa5
Nick Krichevsky <njk828@gmail.com>
2016-07-21 23:22:12 -0400

    Change to new-style string formatting in addpost test

--- tests/tests/addpost.py
+++ tests/tests/addpost.py
@@ -14,7 +14,7 @@ ADD_POST_TITLE = "Add Post - Timpani"
 POST_RANDOM = binascii.hexlify(os.urandom(16)).decode()
 POST_TITLE = "Test post, please ignore."
 POST_BODY = ("This is a test post. "
-    "There is no reason you should be paying attention to it. %s" % POST_RANDOM)
+    "There is no reason you should be paying attention to it. {}".format(POST_RANDOM))
 POST_TAGS = ["test", "post", "selenium"]
 
 def test(driver, username, password):
@@ -25,7 +25,7 @@ def test(driver, username, password):
         .until(expected_conditions.title_contains("Timpani")))
 
     #Check that we were redirected to the login page, as we are not logged in.
-    assert driver.title == LOGIN_TITLE, "Title is %s" % driver.title
+    assert driver.title == LOGIN_TITLE, "Title is {}".format(driver.title)
 
     loginForm = driver.find_element_by_id("login-form")
     usernameField = driver.find_element_by_id("username-field")
@@ -38,7 +38,7 @@ def test(driver, username, password):
         .until_not(expected_conditions.title_is(LOGIN_TITLE)))
 
     #We should have been redirected to the add_post page.
-    assert driver.title == ADD_POST_TITLE, "Title is %s" % driver.title
+    assert driver.title == ADD_POST_TITLE, "Title is {}".format(driver.title)
 
     postForm = driver.find_element_by_id("post-form")
     titleInput = driver.find_element_by_id("title-input")
@@ -77,6 +77,6 @@ def test(driver, username, password):
     tags = [tag[0] for tag in tags]
 
     assert post != None
-    assert post.title == POST_TITLE, "Title is %s" % post.title
+    assert post.title == POST_TITLE, "Title is {}".format(post.title)
     assert POST_RANDOM in post.body
-    assert tags == POST_TAGS, "Tags are %s" % tags
+    assert tags == POST_TAGS, "Tags are {}".format(tags)
